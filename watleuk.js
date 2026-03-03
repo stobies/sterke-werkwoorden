55cdbddd56c541226c57e3b2b4ebc71019e11ab4
Stephan Tobies <stephan@tobies.info>
2026-03-03 19:35:38 +0100

    extended word list, made help text more visible

--- watleuk.js
+++ watleuk.js
@@ -201,5 +201,145 @@ window.WatLeukList = [
         "kozen",
         "gekozen",
         {"en": "choose, select", "de": "wählen, auswählen"}
+    ],
+    [
+        "kijken",
+        "keek",
+        "keken",
+        "gekeken",
+        {"en": "look, watch, view", "de": "schauen, blicken, anschauen"}
+    ],
+    [
+        "klinken",
+        "klonk",
+        "klonken",
+        "geklonken",
+        {"en": "sound, ring, rivet", "de": "klingen, ertönen, tönen"}
+    ],
+    [
+        "komen",
+        "kwam",
+        "kwamen",
+        "gekomen",
+        {"en": "come, arrive", "de": "kommen, ankommen"}
+    ],
+    [
+        "kopen",
+        "kocht",
+        "kochten",
+        "gekocht",
+        {"en": "buy, purchase, get", "de": "kaufen, erwerben, einkaufen"}
+    ],
+    [
+        "krijgen",
+        "kreeg",
+        "kregen",
+        "gekregen",
+        {"en": "get, receive, have", "de": "bekommen, erhalten, kriegen"}
+    ],
+    [
+        "kunnen",
+        "kon",
+        "konden",
+        "gekund",
+        {"en": "can, be able to", "de": "können"}
+    ],
+    [
+        "laten",
+        "liet",
+        "lieten",
+        "gelaten",
+        {"en": "let, allow, have (someone)", "de": "lassen"}
+    ],
+    [
+        "lachen",
+        "lachte",
+        "lachten",
+        "gelachen",
+        {"en": "laugh", "de": "lachen"}
+    ],
+    [
+        "lezen",
+        "las",
+        "lazen",
+        "gelezen",
+        {"en": "read", "de": "lesen"}
+    ],
+    [
+        "liggen",
+        "lag",
+        "lagen",
+        "gelegen",
+        {"en": "lie (be situated)", "de": "liegen"}
+    ],
+    [
+        "lijden",
+        "leed",
+        "leden",
+        "geleden",
+        {"en": "suffer", "de": "leiden"}
+    ],
+    [
+        "lijken",
+        "leek",
+        "leken",
+        "geleken",
+        {"en": "seem, appear", "de": "scheinen"}
+    ],
+    [
+        "lopen",
+        "liep",
+        "liepen",
+        "gelopen",
+        {"en": "walk, run", "de": "laufen"}
+    ],
+    [
+        "melken",
+        "molk",
+        "molken",
+        "gemolken",
+        {"en": "milk", "de": "melken"}
+    ],
+    [
+        "meten",
+        "mat",
+        "maten",
+        "gemeten",
+        {"en": "measure", "de": "messen"}
+    ],
+    [
+        "mijden",
+        "meed",
+        "meden",
+        "gemeden",
+        {"en": "avoid", "de": "meiden"}
+    ],
+    [
+        "moeten",
+        "moest",
+        "moesten",
+        "gemoeten",
+        {"en": "must, have to", "de": "müssen"}
+    ],
+    [
+        "mogen",
+        "mocht",
+        "mochten",
+        "gemogen",
+        {"en": "may, be allowed to", "de": "dürfen"}
+    ],
+    [
+        "nemen",
+        "nam",
+        "namen",
+        "genomen",
+        {"en": "take, get", "de": "nehmen"}
+    ],
+    [
+        "raden",
+        "raadde",
+        "raadden",
+        "geraden",
+        {"en": "guess, advise", "de": "raten"}
     ]
 ];
